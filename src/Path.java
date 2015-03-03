242a03feb1134c6ee0010dccab6a8fbbe91fc988
ahtml <anandhtml@gmail.com>
2015-03-03 18:09:12 -0500

    adding Path.java

---
+++ src/Path.java
@@ -0,0 +1,14 @@
+
+public class Path{
+	
+	public static int length; // this will be used to calculate the entrance and exit of the path
+	int pos;
+	
+	
+	public Path(int pos, int length){
+		this.pos = pos;
+		this.length = length;
+	}
+	
+
+}
