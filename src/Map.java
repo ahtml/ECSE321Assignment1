2ca3e3fe143678f748bac65b045e44ad4c3e69ab
ahtml <anandhtml@gmail.com>
2015-03-03 14:53:14 -0500

    First iteration of Map constructor

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,11 @@
 
 public class Map {
+	
+	public int grid [][]; // This will be an array holding the path and scenery info
+	
+	public Map(int length, int height){
+		
+		grid = new int [length][height];
+	}
 
 }
