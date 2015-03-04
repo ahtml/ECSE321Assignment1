661bcfe47c5dd4dd99e10f45bd219b7b279979ac
ahtml <anandhtml@gmail.com>
2015-03-03 22:58:09 -0500

    Made a few testing parmaters

--- src/driver.java
+++ src/driver.java
@@ -1,8 +1,22 @@
 
 public class driver {
 	
+	
 	public void main(){
 		
 	}
+	
+	
+	public static void main(String [] args){
+		
+		Map m = new Map(10,10);
+		StraightWE p = new StraightWE(9,10);
+		System.out.println(p.getEntrance());
+		System.out.println(p.getExit());
+		System.out.println(p.getpos());
+		System.out.println(p.getisEdge());
+	}
+	
+	
 
 }
