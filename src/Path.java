a006d01d75219b0b253ef1677fb1fffe0d5cb29d
ahtml <anandhtml@gmail.com>
2015-03-04 14:58:41 -0500

    added all the abstract methods which correspond to the subclasses

--- src/Path.java
+++ src/Path.java
@@ -12,6 +12,14 @@ public abstract class Path{
 	
 	// used abstract methods so that Map can call upon Path and the method will be chosen in the 
 	// correct subclass
+	abstract public void rotate();
 	abstract public int getEntrance();
+	abstract public int getExit();
+	abstract public int getpos();
+	abstract public void setisEdge();
+	abstract public boolean getisEdge();
+	abstract public void setEntry();
+	abstract public void setExit();
+	abstract public boolean getedgeType();
 
 }
