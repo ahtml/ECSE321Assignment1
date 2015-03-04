d8da6dd58fa248689d2fd718d0140ff792f2c1b6
ahtml <anandhtml@gmail.com>
2015-03-04 14:43:01 -0500

    changed Path to abstracted and explained this as a comment

--- src/Path.java
+++ src/Path.java
@@ -1,5 +1,5 @@
 
-public class Path{
+public abstract class Path{
 	
 	private int length; // this will be used to calculate the entrance and exit of the path
 	int pos;
@@ -10,5 +10,8 @@ public class Path{
 		this.length = length;
 	}
 	
+	// used abstract methods so that Map can call upon Path and the method will be chosen in the 
+	// correct subclass
+	abstract public int getEntrance();
 
 }
