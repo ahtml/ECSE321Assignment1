4157075bbc069d20b3f85a57c0ba98c998c843a1
ahtml <anandhtml@gmail.com>
2015-03-04 22:22:13 -0500

    insert comment explaining nameing conventions used for the Path subclasses

--- src/Path.java
+++ src/Path.java
@@ -4,7 +4,12 @@ public abstract class Path extends Tile{
 	private int length; // this will be used to calculate the entrance and exit of the path
 	int pos;
 	
-	
+	/*
+	 * Naming all the subclasses is done by using one direction using compass headings
+	 * For example, the horizontal piece comes in from the west and exits the east; called StraightWE
+	 * Instead of creating another subclass called StraightEW for the opposite direction, have a method called rotate which switches the
+	 * entrance and exit variables
+	 */
 	public Path(int pos){
 		super(pos);
 	}
